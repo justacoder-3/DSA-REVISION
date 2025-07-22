6c0a85aac700e6770e99c217ff007e5dee96df21
rama <ramasiva32404@gmail.com>
2025-07-22 21:33:50 +0530

    Create Bellman Ford Algorithm

---
+++ Graphs/BellmanFordAlgorithm.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> bellmanFord (int v, vector<vector<int>> &edges, int s) {
+    vector<int> dist (v, 1e8);
+    dist[s] = 0;
+    for (int i=0; i<v-1; i++) {
+        for (auto it : edges) {
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+                dist[v] = dist[u] + wt;
+            }
+        }
+    }
+
+    for (auto it : edges) {
+        int u = it[0];
+        int v = it[1];
+        int wt = it[2];
+        if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+            return {-1};
+        }
+    }
+
+    return dist;
+}
