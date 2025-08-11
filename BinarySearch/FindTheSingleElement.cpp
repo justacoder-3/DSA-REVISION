7480c6555e2d470b9f90307f91a896666ce9efc4
rama <ramasiva32404@gmail.com>
2025-08-11 15:30:38 +0530

    update

--- BinarySearch/FindTheSingleElement.cpp
+++ BinarySearch/FindTheSingleElement.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// brute force approach 1
+int frequencyCountMethod (vector<int> arr) {
+    int n = arr.size();
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+        for (int j = 0; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+        if (count == 1) {
+            return arr[i];
+        }
+    }
+    return -1;
+}
+
+// time complexity = O(n^2)
+// space complexity = O(1)
+
+int sortArrayB
+
+
+// optimal solution
 int findingTheSingleElements (vector<int> arr) {
     int n = arr.size();
 
