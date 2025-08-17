e7895f856a658c4d62f336ca713671860874c8bd
rama <ramasiva32404@gmail.com>
2025-08-17 07:25:55 +0530

    update

--- BinarySearch/DetectALoop.cpp
+++ BinarySearch/DetectALoop.cpp
@@ -31,7 +31,30 @@ bool detectALoop1 (Node* head) {
 }
 
 // time complexity : O(nlogn) -> if map is used and O(n) -> if unordered map is used
-// in the map -> mpp.find and mpp[temp] that is finding and inserting operations take O(logn) thats why above O(nlogn) whereas in unordered map the same operations take O(n)
+// explanation : in the map -> mpp.find and mpp[temp] that is finding and inserting operations take O(logn) thats why above O(nlogn) whereas in unordered map the same operations take O(n)
+// space complexity : O(n)
+
+// set method
+bool detectALoopSetMethod (Node* head) {
+    unordered_set<Node*> st;
+    Node* temp = head;
+
+    while (temp != NULL) {
+        if (st.find(temp) != st.end()) {
+            return true;
+        }
+        st.insert(temp);
+        temp = temp->next;
+    }
+    return false;
+}
+
+// time complexity : O(n) and O(n^2) in rare worst cases
+// explanation : Operations in unordered_set
+//          insert(x) → O(1) average (because hash table stores it directly in a bucket).
+//          find(x) → O(1) average (just check the bucket for that hash value).
+//          Worst case: If hashing is bad (lots of collisions), both become O(N), but that’s rare with a good hash function.
+//          So, time complexity becomes O(n * n) -> O(n^2). 
 // space complexity : O(n)
 
 // optimal approach (tortoise & hare method)
