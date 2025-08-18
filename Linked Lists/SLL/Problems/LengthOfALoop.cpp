182f47613430b0ac8735f0722665bd1174e1367f
rama <ramasiva32404@gmail.com>
2025-08-18 17:56:38 +0530

    update

--- Linked Lists/SLL/Problems/LengthOfALoop.cpp
+++ Linked Lists/SLL/Problems/LengthOfALoop.cpp
@@ -16,8 +16,8 @@ struct Node {
     }
 };
 
-// normal map method
-int lengthOfALoop (Node* head) {
+// normal map method  
+int lengthOfALoop1 (Node* head) {
     Node* temp = head;
     map <Node*, int> st;
     int count = 1;
@@ -35,3 +35,33 @@ int lengthOfALoop (Node* head) {
 
 // time complexity : O(n)
 // space complexity : O(n)
+
+// tortoise and hare method
+int findLength (Node* slow, Node* fast) {
+    int count = 1;
+    fast = fast->next;
+    while (fast != slow) {
+        count++;
+        fast = fast->next;
+    }
+    return count;
+}
+
+int lengthOfALoop2 (Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast= fast->next->next;
+        if (slow == fast) {
+            return findLength (slow, fast);
+        }
+    }
+    return 0;
+}
+
+// time complexity : O(2n) => O(n)
+// -> Detect cycle with Floyd’s algo (O(N))
+// -> Count length by traversing the cycle once (O(N) worst case).
+// O(2n) becomes O(n) finally becoz constants are ignored in calculating time complexity
+// space complexity : O(1)
