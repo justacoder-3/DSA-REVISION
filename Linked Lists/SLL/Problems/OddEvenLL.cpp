5fb10bc278276175b0d8a154bea5f56b795a1640
rama <ramasiva32404@gmail.com>
2025-08-20 22:07:21 +0530

    update

--- Linked Lists/SLL/Problems/OddEvenLL.cpp
+++ Linked Lists/SLL/Problems/OddEvenLL.cpp
@@ -55,5 +55,25 @@ Node* segregateOddEvenLL1 (Node* head) {
 
 // optimal solution 
 Node* segregateOddEvenLL2 (Node* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
 
+    Node* odd = head;
+    Node* even = head->next;
+    Node* evenHead = head->next;
+
+    while (even != NULL && even->next != NULL) {
+        odd->next = odd->next->next;
+        even->next = even->next->next; // time complexity is O(n/2)
+
+        odd = odd->next;
+        even = even->next; // time complexity is O(n/2)
+    }
+
+    odd->next = evenHead;
+    return head;
 }
+
+// time complexity : O(n/2 + n/2) = O(n) 
+// space complexity : O(n)
