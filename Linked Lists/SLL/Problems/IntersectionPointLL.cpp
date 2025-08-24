a44371875332298ee0a1c6a96861db5a24491d67
rama <ramasiva32404@gmail.com>
2025-08-24 12:50:45 +0530

    update

--- Linked Lists/SLL/Problems/IntersectionPointLL.cpp
+++ Linked Lists/SLL/Problems/IntersectionPointLL.cpp
@@ -18,7 +18,7 @@ struct Node {
 
 // brute force approach
 
-Node* intersectionPoint (Node* head1, Node* head2) {
+Node* intersectionPoint1 (Node* head1, Node* head2) {
 
     if (head1 == NULL || head2 == NULL) {
         return NULL;
@@ -44,8 +44,88 @@ Node* intersectionPoint (Node* head1, Node* head2) {
     return NULL;
 }
 
-// time complexity : O(n*a) + O(m*b) + O(n+m) [where n is the no of nodes in first LL and m is from second LL] => O(1) + O(1) + O(n+m) => O(n+m)
+// time complexity : O(n*a) + O(m*b) [where n is the no of nodes in first LL and m is from second LL] => O(n*1) + O(m*1) => O(n) + O(m) => O(n+m)
 // where a = time complexity taken by unordered_map to insert the nodes into the map => O(1)
 //       b = time complexity taken by unordered_map to compare each and every node with the intersecting node => O(1)
 // in worst cases : O(n*m) [if hash collissions are present]
 // space complexity : O(n) [we store only n nodes in the map where n nodes are the nodes from the first linked list]
+
+// reducing the space complexity approach
+Node* collissionPoint (Node* t1, Node* t2, int d) {
+    while (d) {
+        d--;
+        t2 = t2->next;
+    } // O(n2-n1)
+    while (t1 != t2) {
+        t1 = t1->next;
+        t2 = t2->next;
+    } // O(n1) => assuming n2 is shorter and in worst case it would traverse entire array
+    return t1;
+}
+
+Node* intersectionPoint2 (Node* head1, Node* head2) {
+    Node* t1 = head1;
+    int count1 = 0;
+    Node* t2 = head2;
+    int count2 = 0;
+
+    while (t1 != NULL) {
+        t1 = t1->next;
+        count1++;
+    } // O(n1)
+
+    while (t2 != NULL) {
+        t2 = t2->next;
+        count2++;
+    } // O(n2)
+
+    if (count1 < count2) {
+        return collissionPoint(head1, head2, count2-count1);
+    }
+    else {
+        return collissionPoint(head2, head1, count1-count2);
+    }
+}
+
+// time complexity : O(n1) + O(n2) + O(n2-n1) + O(n1) => O(n1+2n2)
+// space complexity : O(1)
+
+// practice
+Node* collissionPoint1 (Node* t1, Node* t2, int d) {
+    while (d) {
+        d--;
+        t2 = t2->next;
+    }
+
+    while (t1 != t2) {
+        t1 = t1->next;
+        t2 = t2->next;
+    }
+
+    return t1;
+}
+
+Node* intersectionPoint3 (Node* head1, Node* head2) {
+    Node* t1 = head1;
+    Node* t2 = head2;
+    int count1 = 0;
+    int count2 = 0;
+
+    while (t1 != NULL) {
+        count1++;
+        t1 = t1->next;
+    }
+
+    while (t2 != NULL) {
+        count2++;
+        t2 = t2->next;
+    }
+
+    if (count2 > count1) {
+        return collissionPoint1 (head1, head2, count2-count1);
+    }
+
+    else {
+        return collissionPoint1 (head2, head1, count1-count2);
+    }
+}
