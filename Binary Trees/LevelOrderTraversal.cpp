89cb65d59e308040eff6c519ccf5157b047467fc
rama <ramasiva32404@gmail.com>
2025-08-27 11:16:08 +0530

    update

--- Binary Trees/LevelOrderTraversal.cpp
+++ Binary Trees/LevelOrderTraversal.cpp
@@ -11,32 +11,6 @@ struct Node {
     }
 };
 
-// vector<vector<int>> LevelOrderTraversal(Node* root) {
-//     vector<vector<int>> ans;
-//     if (root == NULL) {
-//         return ans;
-//     }
-//     queue<Node*> q;
-//     q.push(root);
-//     while (!q.empty()) {
-//         int size = q.size();
-//         vector<int> level;
-//         for (int i = 0; i < size; i++) {
-//             Node* node = q.front();
-//             q.pop();
-//             if (node->left != NULL) {
-//                 q.push(node->left);
-//             }
-//             if (node->right != NULL) {
-//                 q.push(node->right);
-//             }
-//             level.push_back(node->data);
-//         }
-//         ans.push_back(level);
-//     }
-//     return ans;
-// }
-
 vector<vector<int>> LevelOrderTraversal(Node* root) {
     vector<vector<int>> ans;
     if (root == NULL) {
@@ -60,5 +34,43 @@ vector<vector<int>> LevelOrderTraversal(Node* root) {
         }
         ans.push_back(level);
     }
+    return ans;
+}
+
+// time complexity : O(n)
+// space complexity : O(n)
+
+// practice code
+vector<vector<int>> bfs (Node* root) {
+    vector<vector<int>> ans;
+    queue<Node*> q;
+    q.push(root);
+
+    if (root == NULL) {
+        return ans;
+    }
+
+    while (!q.empty()) {
+        int size = q.size();
+        vector<int> level;
+
+        for (int i=0; i<size; i++) {
+            Node* node = q.front();
+            q.pop();
+
+            if (node->left) {
+                q.push(node->left);
+            }
+
+            if (node->right) {
+                q.push(node->right);
+            }
+
+            level.push_back(node->data);
+        }
+
+        ans.push_back(level);
+    }
+
     return ans;
 }
